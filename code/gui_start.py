115f78717d5e8ac84430637ea9db328c1579ba49
SomeGuyOnTheWeb650 <pollockjosef@gmail.com>
2026-05-22 00:53:54 -0700

    token

--- code/gui_start.py
+++ code/gui_start.py
@@ -3,44 +3,50 @@ from tkinter import ttk
 from organize import *
 from main import *
 
-def build_data(start=None, end=None):
-
-    generate_data_text()
-    
-    # Takes an initial pdf and creates a new file in data_text
-    
-
-    data = []
-    src = pathlib.Path("data_text/")
-
-
-    for file in sorted(src.iterdir()):
-        
-        if start is not None and file.name[5:] < start:
-            continue
-        if end is not None and file.name[5:] > end:
-            continue
-        text = establish_lines(file)
-        chequeing = keep_only_entries_with_transaction(text)
-        
-        data.append(chequeing)
-    # should be the transaction log of chequeing account
-    
-    chunks = []
-    
-    for dat in data:
-        
-        blocks = create_list_of_transactions(dat)
-        
-        chunks.extend(blocks)
-    
-    dictionary = initialize_dictionary(chunks)
-    return dictionary
 
     # woo! got both deposits and withdrawals working, reworked the framework I was using to sort them
     # I definitely know there are better ways, but I got my way working and it feels semi-clean XD
 
 class Window_for_Presentation:
+    def build_data(self, start=None, end=None):
+        start = self.datestart.get()
+        end = self.dateend.get()
+        generate_data_text()
+    
+        # Takes an initial pdf and creates a new file in data_text
+    
+
+        data = []
+        src = pathlib.Path("data_text/")
+
+
+        for file in sorted(src.iterdir()):
+            f = file.name[5:9]
+            
+            
+            if start is not None and f < start:
+                continue
+            if end is not None and f > end:
+                continue
+            text = establish_lines(file)
+            chequeing = keep_only_entries_with_transaction(text)
+            
+            data.append(chequeing)
+        # should be the transaction log of chequeing account
+        
+        chunks = []
+        
+        for dat in data:
+            
+            blocks = create_list_of_transactions(dat)
+            
+            chunks.extend(blocks)
+        
+        dictionary = initialize_dictionary(chunks)
+        # format dictionary here
+        
+        self.data.set(dictionary)
+        
     def __init__(self, root):
 
 
@@ -59,14 +65,15 @@ class Window_for_Presentation:
         end_entry.grid(column=2, row=1, sticky=(W, E))
 
         self.data = StringVar()
-        ttk.Label(mainframe, textvariable=self.data).grid(column=2, row=2, sticky=(W, E))
+        self.deposit = StringVar()
+        self.withdrawal = StringVar()
 
+        ttk.Label(mainframe, textvariable=self.data).grid(column=2, row=4, sticky=W)
 
-        ttk.Button(mainframe, text="Run", command=self.calculate).grid(column=3, row=3, sticky=W)
+        ttk.Button(mainframe, text="Run", command=self.build_data).grid(column=3, row=3, sticky=W)
 
-        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+        ttk.Label(mainframe, text="Date Range (yy/mm)").grid(column=3, row=1, sticky=W)
+
 
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
@@ -75,10 +82,27 @@ class Window_for_Presentation:
         for child in mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
-        feet_entry.focus()
-        root.bind("<Return>", self.calculate)
+        start_entry.focus()
+        root.bind("<Return>", self.build_data)
+    
+    def format_data(self, mainframe, data: dict):
+        line = []
+        new = {}
+        for item in data:
+            if isinstance(data[item], dict):
+                new[item] = data[item] 
+            else:
 
-build_data()
+                line.append(item)
+        i = 2
+        for item in line:
+            
+            ttk.Label(mainframe, textvariable=f"{item}: {data[item]}").grid(column=2, row=i, sticky=(W, E))
+            i += 2
+
+root = Tk()
+Window_for_Presentation(root)
+root.mainloop()
 
 
 
